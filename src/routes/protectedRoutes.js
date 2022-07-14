48152be80de43a2bcd16d04059b313feb87801e8
vitaliikrytchak <vitalii.krytchak@softwarenetic.com>
2022-07-14 13:12:49 +0300

    delete console.log

--- src/routes/protectedRoutes.js
+++ src/routes/protectedRoutes.js
@@ -3,6 +3,5 @@ import { Navigate } from "react-router-dom";
 import Dashboard from "../components/dashboard/dashboard";
 
 export default function ProtectedRoutes({ auth }) {
-  console.log(auth, "from protected route");
   return auth ? <Dashboard /> : <Navigate to="/login" />;
 }
